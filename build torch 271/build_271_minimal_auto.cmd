b7c79501965d48fb08a9e161d2f8dc352ac74f54
Ivan Rodionov <129909368+theIvanR@users.noreply.github.com>
2026-01-23 14:30:53 +0100

    Create build_271_minimal_auto.cmd

---
+++ build torch 271/build_271_minimal_auto.cmd
@@ -0,0 +1,39 @@
+REM Mini Pytorch Builder 2.7.1 
+REM thsi will throw aoti error, patch by updating wheel install with 3 files (or use pre built ones)
+
+
+@echo off
+setlocal EnableDelayedExpansion
+
+REM 1: Initialize Paths
+REM ===============================
+set "SRC_DIR=C:\Users\%USERNAME%\source\271\pytorch"
+
+REM A: Call builders
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" x64
+
+REM B: Initialize CUDA 
+set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin;%PATH%
+set LIB=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\lib\x64;%LIB%
+set INCLUDE=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\include;%INCLUDE%
+
+
+REM 2: Enter Source and Build Wheel
+REM ===============================
+pushd "%SRC_DIR%" || (
+    echo ERROR: Failed to enter %SRC_DIR%
+    exit /b 1
+)
+
+REM Build Options (mkl buggy, change to your cc desired version) 
+set USE_MKL=0
+set USE_MKLDNN=0
+set USE_CUDNN=0
+set "TORCH_CUDA_ARCH_LIST=3.5"
+python -m build --wheel --no-isolation
+
+
+REM ===== Cleanup =====
+popd
+endlocal
+exit /b 0
